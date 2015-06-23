52c528934ca86fe732413a366b7cd560be7cb178
IB <IB@192.168.0.11>
2015-06-23 18:43:55 +0200

    second change

--- GitTest/src/gitpck/Class1.java
+++ GitTest/src/gitpck/Class1.java
@@ -3,8 +3,19 @@ package gitpck;
 public class Class1 {
 
 	public static void main(String[] args) {
+		
+		System.out.println("szíjjad ki");
+		
 		System.out.println("faszom");
+		
+		fuck();
 
+	}
+	
+	public static void fuck()
+	{
+		
+		System.out.println("FUCK U");
 	}
 
 }
